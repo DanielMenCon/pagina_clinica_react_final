858df5ab34c55dc493c37c4049060c1196e980d9
DanielMenCon <daniel.mendez23@inacapmail.cl>
2026-06-23 15:07:44 -0400

    uiltima version

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react' // o @vitejs/react-swc
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pagina_clinica_react_landing/', //  AQUÍ SÍ, en la raíz
+  base: '/pagina_clinica_react_final/', //  AQUÍ SÍ, en la raíz
   plugins: [react()],
 })
